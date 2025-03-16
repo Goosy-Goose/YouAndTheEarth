8736ee3cde1cbd82145b6106cd56bf74a57d514f
Goosy-Goose <selina.gong0406@gmail.com>
2025-03-15 20:03:30 -0400

    Updated movement and tileset implementation
    We now use the input system library to move and the tilesets are now 64 x 64

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,18 +7,34 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rigidBody;
     public float speed;
 
+    InputAction moveAction;
+    InputAction interactAction;
+
     Vector2 direction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Find the references to the "Move" and "Interact" actions
+        moveAction = InputSystem.actions.FindAction("Move");
+        interactAction = InputSystem.actions.FindAction("Interact");
     }
 
     // Update is called once per frame
     void Update()
     {
-        direction = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
-        rigidBody.linearVelocity = direction * speed;
+        MovePlayer();
+    }
+
+
+    void MovePlayer()
+    {
+        // Movement code
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+            //Debug.Log(moveValue);
+        rigidBody.linearVelocity = moveValue * speed ;
+
+        // Interact code
+        bool interactValue = interactAction.IsPressed();
     }
 }
